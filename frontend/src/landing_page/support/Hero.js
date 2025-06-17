b886fe5f9dfec9f322e16b918dfdab6dad37d398
Mushirul Hassan <mushirulhassan2@gmail.com>
2025-06-18 02:28:27 +0530

    Added Navbar, Footer, and updated Hero section layout

--- frontend/src/landing_page/support/Hero.js
+++ frontend/src/landing_page/support/Hero.js
@@ -21,14 +21,14 @@ function Hero() {
         </div>
         <div className="col-6 p-3">
           <h1 className="fs-3">Featured</h1>
-          <ol>
+          <ul>
             <li>
               <a href="">Current Takeovers and Delisting - January 2024</a>
             </li>
             <li>
               <a href="">Latest Intraday leverages - MIS & CO</a>
             </li>
-          </ol>
+          </ul>
         </div>
       </div>
     </section>
